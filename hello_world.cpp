e53a382b46c10941d084eb49dac2871e6c9079a6
ISHTIAQ AHMED <iashtiaqkhattak@gmail.com>
2026-01-29 21:55:42 +0500

    Create hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,10 @@
+cat <<EOF > hello_world.cpp
+#include <iostream>
+
+// Standard C++ main function
+int main() {
+    // This prints text to the Linux terminal
+    std::cout << "Hello from the Linux Repository!" << std::endl;
+    return 0;
+}
+EOF
